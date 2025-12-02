df0c030311cc02c66a2bd67c92b9fe951bc8ce3f
Your Name <youremail@example.com>
2025-12-02 10:23:44 +0300

    finished assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    try:
+        with open(f"{file_name}.txt", "r") as f:
+            return f.read()
+    except FileNotFoundError:
+        return None
